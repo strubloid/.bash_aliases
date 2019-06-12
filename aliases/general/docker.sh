bf4a8e3a16005de2ebdd996ba3a64514e8f28605
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2019-06-12 11:59:59 +0100

    adding new things

--- aliases/general/docker.sh
+++ aliases/general/docker.sh
@@ -6,6 +6,8 @@ alias dk-query-w='watch -n 1 docker-compose exec database mysql -u root -proot -
 
 alias dk-processlist='docker-compose exec database mysqladmin -u root -proot -i 1 processlist'
 alias dk-m2dump="docker-compose exec database sh -c 'mysqldump -h localhost -u root -proot magento --single-transaction' > dump.sql"
+alias dk-start="docker stop \$(docker ps -q) && docker-sync-stack start"
+alias dk-redis-flush="docker-compose exec redis sh -c 'redis-cli flushall'"
 
 # docker-compose exec database mysql -u root -proot -e "SELECT * FROM sales_flat_order ORDER BY created_at DESC LIMIT 2"
 # watch -n 1 'docker-compose exec database mysql -u root -proot magento -e "SELECT entity_id, customer_email, status, created_at,updated_at, base_total_invoiced FROM sales_flat_order ORDER BY created_at DESC LIMIT 2"'
--- aliases/general/git.sh
+++ aliases/general/git.sh
@@ -1,2 +1,3 @@
 # git aliases
 alias git-revert="git clean -d -f -f"
+alias gitup-master="git checkout master && git pull origin master && git fetch --all"
