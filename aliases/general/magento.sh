bb88ace89b894f2ff2d275636f99194cd2a53c8d
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2019-06-19 10:32:49 +0100

    adding new magento aliases

--- aliases/general/magento.sh
+++ aliases/general/magento.sh
@@ -13,3 +13,9 @@ alias magerun-cron-run="magerun sys:cron:run"
 
 ## alias to send email each 10 seconds
 alias magento-send-email='watch -n 10 magerun sys:cron:run core_email_queue_send_all'
+
+
+## m1 how to clean the caches
+alias magerun-cache-clean="docker-compose exec php sh -c 'n98-magerun cache:clean'"
+alias magerun-cache-flush="docker-compose exec php sh -c 'n98-magerun cache:flush'"
+alias magerun-cache-all="docker-compose exec php sh -c 'n98-magerun cache:flush && n98-magerun cache:clean'"
