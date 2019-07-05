1e781f10b79846938528ab86733b1edaead1ae43
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2019-07-05 08:51:40 +0100

    adding new aliases

--- aliases/general/docker.sh
+++ aliases/general/docker.sh
@@ -1,5 +1,5 @@
 ## docker main commands
-alias dk-start="docker stop \$(docker ps -q) && docker-sync-stack start"
+alias dk-stop-start="docker stop \$(docker ps -q) && docker-sync-stack start"
 
 ## cache related
 alias dk-redis-flush="docker-compose exec redis sh -c 'redis-cli flushall'"
@@ -16,9 +16,17 @@ alias dk-database-query-watch='watch -n 1 docker-compose exec database mysql -u 
 alias dk-database-dump="docker-compose exec database sh -c 'mysqldump -h localhost -u root -proot magento --single-transaction' > dump.sql"
 alias dk-database-processlist='docker-compose exec database mysqladmin -u root -proot -i 1 processlist'
 
-alias dk-create-admin-user="docker-compose exec php sh -c \"bin/magento admin:user:create --admin-user='rafael' --admin-password='rafa1234' --admin-email='rafael@monsoonconsulting.com' --admin-firstname='Rafael' --admin-lastname='Mendes' \""
+alias dk-create-admin-user="docker-compose exec php sh -c \"php bin/magento admin:user:create --admin-user='rafael' --admin-password='rafa1234' --admin-email='rafael@monsoonconsulting.com' --admin-firstname='Rafael' --admin-lastname='Mendes' \""
 
 # docker-compose exec database mysql -u root -proot -e "SELECT * FROM sales_flat_order ORDER BY created_at DESC LIMIT 2"
 # watch -n 1 'docker-compose exec database mysql -u root -proot magento -e "SELECT entity_id, customer_email, status, created_at,updated_at, base_total_invoiced FROM sales_flat_order ORDER BY created_at DESC LIMIT 2"'
 
 alias dk-update="sudo chmod 777 ~/.docker/ -Rf && sudo chown strubloid:strubloid  ~/.docker/ -Rf"
+
+# this will start the docker sync and the container
+alias dk-start="docker-sync start && docker-compose up -d"
+
+
+# docker-compose exec database mysql -u root -proot -e "select * from magento.catalogsearch_result"
+# docker-compose exec database mysql -u root -proot -e "cat .docker/select.sql" > 2019-07-03-select.txt
+
--- aliases/general/magento2.sh
+++ aliases/general/magento2.sh
@@ -8,6 +8,13 @@ alias m2-project-developer="bin/magento deploy:mode:set developer"
 
 alias m2-create-admin-user="bin/magento admin:user:create --admin-user='rafael' --admin-password='rafa1234' --admin-email='rafael@monsoonconsulting.com' --admin-firstname='Rafael' --admin-lastname='Mendes'"
 
+alias m2-template-on="docker-compose exec php sh -c 'php bin/magento dev:template-hints:enable'"
+alias m2-template-off="docker-compose exec php sh -c 'php bin/magento dev:template-hints:disable'"
+
+alias m2-cache-clean="docker-compose exec php sh -c 'php bin/magento cache:clean'"
+alias m2-cache-flush="docker-compose exec php sh -c 'php bin/magento cache:flush'"
+alias m2-cleanall="docker-compose exec php sh -c 'php bin/magento cache:clean && php bin/magento cache:flush'"
+
 ## how to generate the mapping for all xml files
 # bin/magento dev:urn-catalog:generate .idea/misc.xml
 # docker-compose exec php bin/magento dev:urn-catalog:generate .idea/misc.xml
