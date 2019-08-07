94d0532f5dd90348d840cba1a02c8cb4a556a040
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2019-08-07 15:11:59 +0100

    fixing new magento 1 alias to work with docker

--- aliases/general/magento.sh
+++ aliases/general/magento.sh
@@ -1,19 +1,21 @@
 ## magento aliases ##
 
 ## magento helpers
-alias magerun-setup-incremental='magerun sys:setup:incremental'
-alias magerun-setup-run='magerun sys:setup:run'
-alias magerun-template-hints='magerun dev:template-hints'
-alias magerun-create-user='magerun admin:user:create rafael.mendes rafael.mendes@monsoonconsulting.com r@f@1234 rafael mendes'
-alias magerun-apply-price-rules='magerun sys:cron:run catalogrule_apply_all'
+alias magerun-setup-incremental="docker-compose exec php sh -c 'n98-magerun sys:setup:incremental'"
+alias magerun-setup-run="docker-compose exec php sh -c 'n98-magerun sys:setup:run'"
+alias magerun-template-hints="docker-compose exec php sh -c 'n98-magerun dev:template-hints'"
+
+## Check this: https://unix.stackexchange.com/questions/3773/how-to-pass-parameters-to-an-alias
+## alias wrap_args='f(){ echo before "$@" after;  unset -f f; }; f'
+## alias magerun-apply-price-rules='magerun sys:cron:run catalogrule_apply_all'
 
 ## magerun cron commands
-alias magerun-cron-list="magerun sys:cron:list"
-alias magerun-cron-run="magerun sys:cron:run"
+alias magerun-cron-list="docker-compose exec php sh -c 'n98-magerun sys:cron:list'"
+alias magerun-cron-run="docker-compose exec php sh -c 'n98-magerun sys:cron:run'"
 
-## alias to send email each 10 seconds
-alias magento-send-email='watch -n 10 magerun sys:cron:run core_email_queue_send_all'
-
+# sending email commands
+alias magento-send-email="docker-compose exec php sh -c 'n98-magerun sys:cron:run core_email_queue_send_all'"
+alias magento-send-email-10="watch -n 10 docker-compose exec php sh -c 'n98-magerun sys:cron:run core_email_queue_send_all'"
 
 ## m1 how to clean the caches
 alias magerun-cache-clean="docker-compose exec php sh -c 'n98-magerun cache:clean'"
@@ -21,7 +23,7 @@ alias magerun-cache-flush="docker-compose exec php sh -c 'n98-magerun cache:flus
 alias magerun-cache-all="docker-compose exec php sh -c 'n98-magerun cache:flush && n98-magerun cache:clean'"
 
 alias magerun-reindex-all="docker-compose exec php sh -c 'n98-magerun index:reindex:all'"
-
+alias magerun-create-user="docker-compose exec php sh -c 'n98-magerun admin:user:create rafael.mendes rafael.mendes@monsoonconsulting.com rafa1234 rafael mendes'"
 
 
 # docker-compose exec php export PHP_IDE_CONFIG="serverName=xdebug" && php -dxdebug.profiler_enable=1 -dxdebug.remote_autostart=1 shell/removeCustomers.php
