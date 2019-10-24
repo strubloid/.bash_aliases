a1911404f2a940e51b541dcb0e7e5b95fd32c219
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2019-10-24 14:03:49 +0100

    adding new things

--- aliases/general/magerun2.sh
+++ aliases/general/magerun2.sh
@@ -1,12 +1,11 @@
 # magerun 2
-
-magerun2-enable()
+m2-enable()
 {
     wget https://files.magerun.net/n98-magerun2.phar
-    chmod +x ./n98-magerun2.phar
+    sudo chmod +x ./n98-magerun2.phar
 }
 
-magerun2-disable()
+m2-disable()
 {
     rm n98-magerun2.phar
 }
