997ccbf1ecd7a71f75f508e03f7f82044f4d075a
rafael.mendes <rafael.mendes@monsoonconsulting.com>
2020-03-10 12:05:36 +0000

    new things

--- aliases/general/moma.sh
+++ aliases/general/moma.sh
@@ -268,16 +268,16 @@ moma-cache-flush()
 moma-varnish-flush()
 {
   if [ -n "$2" ]; then
-      moma-dk-varnish-exec "varnishadm -T 127.0.0.1:$2 -S /etc/varnish/secret ban .req.http.host == $1'"
+      moma-dk-varnish-exec "varnishadm -T 127.0.0.1:$2 -S /etc/varnish/secret ban '.req.http.host == $1'"
   else
-      moma-dk-varnish-exec "varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret ban .req.http.host == $1'"
+      moma-dk-varnish-exec "varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret ban '.req.http.host == $1'"
   fi
 }
 
 ## This will clean the varnish cache for centralbank
 moma-varnish-centralbank-flush()
 {
-  moma-dk-varnish-exec "varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret ban .req.http.host == collectorcoins.ie.test'"
+  moma-dk-varnish-exec "varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret ban '.req.http.host == collectorcoins.ie.test'"
 }
 
 # Method that will run the cache:flush for the full_page cache
