d92a62b99ea4d906508f7b353ebc5273042a0189
strubloid <joserafaelmb@gmail.com>
2020-07-23 13:29:02 +0100

    test

--- aliases/general/git.sh
+++ aliases/general/git.sh
@@ -10,4 +10,14 @@ alias gitup-master="git checkout master && git pull origin master && git fetch -
 # git basic commands
 alias gc="git commit -m"
 alias gs="git status"
-alias gpush="git commit -m $1 && git push origin master"
+
+gitpush()
+{
+    if [ -z "$1" ]
+    then
+      printf "[ERR]: You must pass an argument to use this function"
+    else
+      printf "Git message: $1\n"
+      git commit -m "$1" && git push origin master
+    fi
+}
