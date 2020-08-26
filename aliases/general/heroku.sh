38d5d84729ce5d5d8a317a7c27a134953eb6dc37
strubloid <strubloid@gmail.com>
2020-08-26 18:24:50 +0100

    updating mongo commands

--- aliases/general/heroku.sh
+++ aliases/general/heroku.sh
@@ -2,4 +2,19 @@
 
 # Strubloid::general::heroku
 
-alias h-login="heroku login --interactive"
+alias heroku-login="heroku login"
+alias heroku-login-shell="heroku login --interactive"
+alias heroku-push="git push heroku master"
+
+
+# Adding a heroku repository to the current git project
+heroku-add-repository()
+{
+  heroku git:remote -a "$1"
+}
+
+# Basic steps to install heroku on ubuntu
+heroku-install()
+{
+  sudo snap install --classic heroku
+}
--- aliases/general/mongo.sh
+++ aliases/general/mongo.sh
@@ -6,3 +6,5 @@ alias m-s="brew services start mongodb-community"
 
 alias mo-basic-structure-dev="npm i --save-dev nodemon"
 alias mo-basic-structure="npm i express mongoose ejs marked slugify method-override dompurify jsdom"
+
+alias mongo-mygitbook="mongo 'mongodb+srv://mygitbook.0yzyd.mongodb.net/mygitbook' --username strubloid"
