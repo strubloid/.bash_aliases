8715e8608903075abf1eec180065be481e2ebbcb
strubloid <joserafaelmb@gmail.com>
2020-08-28 18:48:48 +0100

    adding the missing gitignore and new package .json model files on import folder

--- aliases/general/heroku.sh
+++ aliases/general/heroku.sh
@@ -6,13 +6,30 @@ alias heroku-login="heroku login"
 alias heroku-login-shell="heroku login --interactive"
 
 # Adding a heroku repository to the current git project
-heroku-add-repository()
-{
+heroku-add-repository() {
   heroku git:remote -a "$1"
 }
 
 # Basic steps to install heroku on ubuntu
-heroku-install()
-{
+heroku-install() {
   sudo snap install --classic heroku
+}
+
+
+create-heroku-nodejs-project() {
+
+
+  if [ -n "$1" ]; then
+    git clone https://github.com/mars/heroku-cra-node.git $1 && \
+    cd $1 && \
+    rm -rf LICENSE README.md .git .gitignore && \
+    rm package.json
+
+
+
+  else
+    echo "You must specify the project name\n"
+  fi
+
+
 }
