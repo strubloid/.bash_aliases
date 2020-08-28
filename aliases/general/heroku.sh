b8e2b9de578a1e8a9cb8f83182aa0f0b6053a2f4
strubloid <joserafaelmb@gmail.com>
2020-08-28 21:38:52 +0100

    adding new things for heroku and node applications

--- aliases/general/heroku.sh
+++ aliases/general/heroku.sh
@@ -15,26 +15,63 @@ heroku-install() {
   sudo snap install --classic heroku
 }
 
-create-heroku-nodejs-project() {
-
+heroku-node-new()
+{
   if [ -n "$1" ]; then
-    git clone https://github.com/mars/heroku-cra-node.git $1
+
+    npx create-react-app $1
     cd $1
-    rm -rf LICENSE README.md .git .gitignore
-    rm package.json
-    rm package-lock.json
 
-    # creating a variable just to store where is the package.json
-    PACKAGEJSON=$BASH_ALIASES_PROJECT_FOLDER/import/package.json
+    gitignore=$BASH_ALIASES_PROJECT_FOLDER/import/.gitignore
+    echo -e "[]: Copying $gitignore"
+    cp "$gitignore" .
 
-    echo -e "[]: Copying $PACKAGEJSON"
-    cp "$PACKAGEJSON" .
+    until read -r -p "Git URL (Mandatory): " gitrepositoryurl && test "$gitrepositoryurl" != ""; do
+      continue
+    done
 
-    perl -i -pe 's/"\[app-name\]"/"'$1'"/g' $PACKAGEJSON
+    printf "Git URL: $gitrepositoryurl\n"
+    git remote add origin $gitrepositoryurl
+    git branch -M master
+    git push -u origin master
+    git add .
+    git commit -m "Starting new project: $1"
+    git push origin master
+
 
   else
 
     echo "You must specify the project name\n"
   fi
-
 }
+
+
+#create-heroku-nodejs-project() {
+#
+#  if [ -n "$1" ]; then
+#    git clone https://github.com/mars/heroku-cra-node.git $1
+#    cd $1
+#    rm -rf LICENSE README.md .git .gitignore
+#    rm package.json
+#    rm package-lock.json
+#
+#    # creating a variable just to store where is the package.json
+#    PACKAGEJSON=$BASH_ALIASES_PROJECT_FOLDER/import/package.json
+#
+#    echo -e "[]: Copying $PACKAGEJSON"
+#    cp "$PACKAGEJSON" .
+#
+#    perl -i -pe 's/"\[app-name\]"/"'$1'"/g' $PACKAGEJSON
+#
+#    # git push heroku master
+#    # heroku buildpacks
+#    # heroku config:set JS_RUNTIME_TARGET_BUNDLE='/app/react-ui/build/static/js/*.js'
+#    # git commit --allow-empty -m 'Enable runtime config with create-react-app-inner-buildpack'
+#    # git push heroku master
+#
+#  else
+#
+#    echo "You must specify the project name\n"
+#  fi
+#
+#}
