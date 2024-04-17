80ecfc872637b7873e30d52b7b5ee1a4ad6adde6
strubloid <joserafaelmb@gmail.com>
2024-04-17 08:11:40 +0100

    fixing the way to build with pip on linux, documented with echos of the how to do so

---
+++ aliases/linux/kdenlive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Strubloid::linux::alienware
+
+## how-to-install-pip3-packages-on-kdenlive
+how-to-install-pip3-packages-on-kdenlive()
+{
+  echo "1 change the source:"
+  echo "source ~/.local/share/kdenlive/venv/bin/activate"
+  echo "run pip3 install srt vosk"
+}
