e4e4d442867fb8c6442d8b577ba220506fbd505d
strubloid <joserafaelmb@gmail.com>
2024-04-26 22:20:55 +0100

    pushing new obs function

---
+++ aliases/linux/obs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Strubloid::linux::obs
+
+# Set the source and target directories
+function loadObsPluginsRelations()
+{
+  SOURCE_DIR="/usr/share/obs/obs-plugins"
+  TARGET_DIR="/home/zero/.var/app/com.obsproject.Studio/config/obs-studio/plugins"
+
+  echo "[Started]: loadObsPluginsRelations"
+
+  # Check if the target directory exists; create it if not
+  if [ ! -d "$TARGET_DIR" ]; then
+      mkdir -p "$TARGET_DIR"
+      echo "[$TARGET_DIR]: created folder"
+  fi
+
+  # Loop through each plugin folder in the source directory
+  for plugin_folder in "$SOURCE_DIR"/*; do
+      # Extract the plugin name from the folder path
+      plugin_name=$(basename "$plugin_folder")
+
+      # Check if the symbolic link already exists
+      if [ ! -e "$TARGET_DIR/$plugin_name" ]; then
+          ln -s "$plugin_folder" "$TARGET_DIR/$plugin_name" # Create a symbolic link
+          echo "[$plugin_name]: Created "
+      else
+          echo "[$plugin_name]: Exist already"
+      fi
+  done
+
+  echo "[Finished] loadObsPluginsRelations"
+}
