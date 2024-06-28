0809102c68d747c545476eb1586c4503f2188f23
strubloid <joserafaelmb@gmail.com>
2024-06-28 16:48:44 +0100

    update knowledge of nvidia, how to setup quickly by terminal the profiles

--- aliases/linux/nvidia.sh
+++ aliases/linux/nvidia.sh
@@ -7,3 +7,26 @@ function upgrade-configuration()
   sudo nvidia-smi -i 0 -pl 100
   sudo nvidia-xconfig
 }
+
+
+# This will enable the first core of the gpu
+# to use one of the following:
+# GPUPowerMizerMode:
+# 0: Auto (default, adaptive mode)
+# 1: Prefer Maximum Performance
+# 2: Prefer Consistent Performance (deprecated in some drivers)
+function nvidia-max-performance()
+{
+  sudo nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
+}
+
+# This will enable the first core of the gpu
+# to use one of the following:
+# GPUPowerMizerMode:
+# 0: Auto (default, adaptive mode)
+# 1: Prefer Maximum Performance
+# 2: Prefer Consistent Performance (deprecated in some drivers)
+function nvidia-auto-performance()
+{
+  sudo nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=0'
+}
