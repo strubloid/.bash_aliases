14c43bd9362f8c0817536b7e735e6f97caa73695
Strubloid <strubloid@gmail.com>
2026-03-24 22:37:42 +0000

    updating the install of the local chatgpt

--- aliases/linux/ai.sh
+++ aliases/linux/ai.sh
@@ -2,24 +2,14 @@
 
 # Strubloid::linux::ai
 
-
-install-chat-gpt-local()
-{
-
-  if [ -z "$1" ]
-  then
-      # shellcheck disable=SC2034
-      # read -p "[Folder to install]: " FOLDER_TO_INSTALL
-      FOLDER_TO_INSTALL="~/apps/gpt-ui"
-  else
-      FOLDER_TO_INSTALL=$1
-  fi
-
-  docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:"$FOLDER_TO_INSTALL" --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
-}
-
-
+# Function to start the chatGPT GUI locally with GPU support
 chat-gpt-gui-local()
 {
-  docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:~/apps/gpt-ui --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
+  docker run -d -p 3000:8080 --gpus all --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
+}
+
+# Function to start the chatGPT GUI locally without GPU support
+chat-gpt-gui-local-cpu()
+{
+  docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 }
