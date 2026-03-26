ee268b00057a9c2b20b9596a44eb87dc33326667
Strubloid <strubloid@gmail.com>
2026-03-26 23:26:22 +0000

    updating chat things

--- aliases/linux/ai.sh
+++ aliases/linux/ai.sh
@@ -3,67 +3,68 @@
 # Strubloid::linux::ai
 
 ## function to start the open web ui
-chat-gpt-gui(){
-  docker start open-webui
-}
 
+## installing the chat cuda version
 chat-install-cuda(){
+
+    # installing the cuda version
     docker run -d \
                -p 3000:8080 \
               --name open-webui \
               --add-host=host.docker.internal:host-gateway \
               -e OLLAMA_BASE_URL=http://host.docker.internal:11434 ghcr.io/open-webui/open-webui:cuda
+    
+    ## creating network
+    docker run -d \
+              --network=host \
+              -e OLLAMA_BASE_URL=http://localhost:11434 ghcr.io/open-webui/open-webui:cuda
 
 }
 
+## installing the chat main version
 chat-install-main(){
+
+    # installing the main version
     docker run -d \
                -p 3000:8080 \
               --name open-webui \
               --add-host=host.docker.internal:host-gateway \
               -e OLLAMA_BASE_URL=http://host.docker.internal:11434 ghcr.io/open-webui/open-webui:main
 
+    ## creating network
+    docker run -d \
+              --network=host \
+              -e OLLAMA_BASE_URL=http://localhost:11434 ghcr.io/open-webui/open-webui:main
+
 }
 
+# command to update the repo
 pull-main-repo(){
   docker pull ghcr.io/open-webui/open-webui:main
 }
 
-
+# command to update the repo
 pull-cuda-repo(){
   docker pull ghcr.io/open-webui/open-webui:cuda
 }
+
 # Function to start the chatGPT GUI locally with GPU support
-chat-gpt-gui-local()
+chat-start()
 {
-  if docker ps -a --format '{{.Names}}' | grep -q '^open-webui$'; then
-    echo "Container exists. Starting..."
-    docker start open-webui
-  else
-    chat-install-cuda
-  fi
+  docker start open-webui
 }
 
 # Function to update the chatGPT GUI locally with GPU support
 update-chat-gpt-gui-local(){
 
-  docker rm -f open-webui                         # 1. Stop and remove the container (data in the volume is preserved)
-  docker pull ghcr.io/open-webui/open-webui:cuda  # 2. Pull the latest image
+  # 1. Stop and remove the container (data in the volume is preserved)
+  docker rm -f open-webui
+  
+  # 2. Pull the latest image
+  pull-cuda-repo
 
   # 3. Recreate the container
   chat-install-cuda
-}
-
-# Function to start the chatGPT GUI locally without GPU support
-chat-gpt-gui-local-cpu()
-{
-    if docker ps -a --format '{{.Names}}' | grep -q '^open-webui$'; then
-    echo "Container exists. Starting..."
-    docker start open-webui
-  else
-    chat-install-main
-  fi
-  
 }
 
 # Function to start the chatGPT GUI locally with GPU support
@@ -72,7 +73,7 @@ update-chat-gpt-gui-local-cpu(){
   docker rm -f open-webui
 
   # 2. Pull the latest image
-  docker pull ghcr.io/open-webui/open-webui:main
+  pull-main-repo
 
   # 3. Recreate the container
   chat-install-main
